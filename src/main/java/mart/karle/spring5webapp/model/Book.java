9e14a6c53225f0016dcdb63cd728e27c308f8d19
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2018-07-24 23:15:45 +0200

    Copyright

--- src/main/java/mart/karle/spring5webapp/model/Book.java
+++ src/main/java/mart/karle/spring5webapp/model/Book.java
@@ -1,3 +1,9 @@
+/*
+ * Developed by carlosmartinez.
+ * Last modified 24/07/18 23:13.
+ * Copyright (c) 2018. All rights reserved.
+ */
+
 package mart.karle.spring5webapp.model;
 
 import java.util.HashSet;
--- src/main/java/mart/karle/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/mart/karle/spring5webapp/repositories/PublisherRepository.java
@@ -1,3 +1,9 @@
+/*
+ * Developed by carlosmartinez.
+ * Last modified 24/07/18 23:13.
+ * Copyright (c) 2018. All rights reserved.
+ */
+
 package mart.karle.spring5webapp.repositories;
 
 import mart.karle.spring5webapp.model.Publisher;
